9f5d6347a0ac7d7c5ede69f51e382608c0246df7
Cesar <cesaramuroc@gmail.com>
2026-01-26 18:26:41 -0400

    update

--- components/home-promo-banners.tsx
+++ components/home-promo-banners.tsx
@@ -23,8 +23,8 @@ export function PromoStackBanners() {
   const currentBanner = bannerByLang[language] || bannerByLang.es;
 
   return (
-    <div className="w-full md:h-[520px] lg:h-[437px] flex flex-col gap-4">
-      <div className="flex-1 relative bg-black">
+    <div className="w-full h-[260px] md:h-[520px] lg:h-[437px] flex flex-col gap-4 overflow-hidden">
+      <div className="flex-1 min-h-0 relative bg-black overflow-hidden">
         <a
           href="https://chileadictohoteles.cl/"
           target="_blank"
@@ -34,17 +34,17 @@ export function PromoStackBanners() {
           <img
             src={currentBanner.src}
             alt={currentBanner.alt}
-            className="object-contain object-center w-full h-full"
+            className="object-cover object-top w-full h-full"
           />
         </a>
       </div>
 
-      <div className="flex-1 relative bg-black">
+      <div className="flex-1 min-h-0 relative bg-black overflow-hidden">
         <Link href="/cafes" className="block w-full h-full">
           <img
             src={currentBanner.src}
             alt="Cafés"
-            className="object-contain object-center w-full h-full"
+            className="object-cover object-bottom w-full h-full"
             loading="lazy"
           />
         </Link>

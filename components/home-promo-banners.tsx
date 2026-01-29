b809b7cf01d0ec376a05c9ade077766a094424cd
Cesar <cesaramuroc@gmail.com>
2026-01-28 20:14:55 -0400

    i

--- components/home-promo-banners.tsx
+++ components/home-promo-banners.tsx
@@ -6,7 +6,12 @@ export function PromoStackBanners() {
   return (
     <div className="w-full h-[260px] md:h-[520px] lg:h-[437px] flex flex-col gap-4 overflow-hidden">
       <div className="flex-1 min-h-0 relative bg-black overflow-hidden">
-        <Link href="/categoria/hoteles" className="block w-full h-full">
+        <Link
+          href="https://chile-adicto-hoteles-front.vercel.app/"
+          className="block w-full h-full"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             src="/70 HOTELES.svg"
             alt="Hoteles"
